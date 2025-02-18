f6850271ba5c7d42a1a1e4952aa747005fd5fd4b
André Robles <155944246+andreroblesb@users.noreply.github.com>
2025-02-18 12:08:37 -0600

    [UPDATE] BFS and DFS completed

--- magicTriangle.py
+++ magicTriangle.py
@@ -1,14 +1,11 @@
 """
-magic triangle problem. We have the triangle below and must fill the nodes with the numbers 1 to 6, in ascendent order, so that the vertices sum 10. Search entity must move along the graph asigning the numbers.
-
-We will use the two most popular uninformed search algorithm (BFS & DFS).
+magic triangle problem. We have the triangle below and must fill the nodes with the numbers 1 to 6, in ascendent order, so that the vertices sum 10. Search entity must therefore not move along the explicit graph, but search on the space of available nodes.
 
 We are assuming:
-- We can go back from node to node
-- We can reasign the number of a node
-- Numbers are put in ascending order
+- All nodes should have assigned a number from 1 to 6 in order to the result to be valid.
 - ALL NODES ARE CONNECTED (implicitly)
 
+We will use the two most popular uninformed search algorithm (BFS & DFS).
 """
 # problem graph given is:
 r'''
@@ -22,17 +19,60 @@ D-----E-----F
 '''
 # is it overengineered? yes, because the understanding of the assignment changed during the development of the solution. Graph does not apply in the traditional sense.
 
+class Queue():
+    def __init__(self, id):
+        self.numbers : list[int] = [x for x in range(1, 7)]
+        self.queue : list[str] = []
+        self.graph : Graph = Graph(A=0, B=0, C=0, D=0, E=0, F=0)
+        self.current : str = None
+        self.id : str = id
+    
+    def update(self):
+        # add moves to queue
+        options : list = self.graph.return_unassigned()
+        for node in options:
+            self.queue.append(node)
+    
+    def check(self):
+        # check if done
+        if len(self.numbers) == 0:
+            if self.graph.constraint_proof():
+                return True, self.graph
+            else:
+                print("Configuration does not pass constraint.")
+                self.numbers.append(self.graph.nodes[self.current])
+                self.graph.nodes[self.current] = 0
+        return None, None 
+             
+    def move(self):          
+        # assign first-in
+        number = self.numbers.pop()
+        self.current = self.queue.pop(0)
+        if self.graph.nodes[self.current] != 0:
+            self.numbers.append(self.graph.nodes[self.current])
+        self.graph.nodes[self.current] = number
+        print(f"Assigned {number} to {self.current}")
+
 # BFS search entity
 class BFS():
-    def __init__(self, start, graph):
-        self.numbers : list[int] = [x for x in range(1, 7)]
-        self.stack : list[str] = []
-        self.graph : Graph = graph
+    def __init__(self, queues):
+        self.queues : list[Queue] = queues
         self.done : bool = False
-        self.current : str = None
+        self.current : Queue = None
     
     def solve(self):
-        
+        print("Starting BFS search")
+        # add moves to queue
+        while not self.done:
+            for branch in self.queues:
+                branch.update()
+                flag, graph = branch.check()
+                if flag:
+                    print("\n First succesfully completed queue found!")
+                    print(graph)
+                    self.done = True
+                    break
+                branch.move()
 
 class DFS():
     def __init__(self, graph):
@@ -49,29 +89,34 @@ class DFS():
         # add moves to stack
         options : list = self.graph.return_unassigned()
         for node in options:
-            self.stack.append(node)
-        while not self.done:
+            self.stack.append(node)     
+   
+        while not self.done: 
             # check if done
             if len(self.numbers) == 0:
                 if self.graph.constraint_proof():
                     self.done = True
                     print("Done!")
+                    print(self.graph)
                     continue
                 else:
-                    print("Configuration does not pass constraint.")
                     self.numbers.append(self.graph.nodes[self.current])
                     self.graph.nodes[self.current] = 0
+                    print(f"Constraint not met")
+                    continue
             
             # assign last-in
-            number = self.numbers.pop()
             self.current = self.stack.pop()
+            number = self.numbers.pop()
+            if self.graph.nodes[self.current] != 0:
+                self.numbers.append(self.graph.nodes[self.current])
             self.graph.nodes[self.current] = number
             print(f"Assigned {number} to {self.current}")
             
             # add moves to stack
             options : list = self.graph.return_unassigned()
             for node in options:
-                self.stack.append(node)
+                self.stack.append(node)  
 
 class Graph():
     def __init__(self, **kwargs : int):
@@ -96,10 +141,25 @@ class Graph():
             printing += f"{node} : {value} \n"
         return printing
 
-# setting the problem graph
-graph = Graph(A=0, B=0, C=0, D=0, E=0, F=0)
+def main():
+    # setting the problem graph
+    graph = Graph(A=0, B=0, C=0, D=0, E=0, F=0)
 
-# perform BFS
-search_entity = DFS(graph)
-search_entity.solve()
-print(graph)
+    # perform DFS
+    search_entity = DFS(graph)
+    search_entity.solve()
+
+    # perform  BFS
+    queues = [
+        Queue(id="1"), 
+        Queue(id="2"), 
+        Queue(id="3"), 
+        Queue(id="4"), 
+        Queue(id="5"), 
+        Queue(id="6")
+    ]
+    search_entity = BFS(queues)
+    search_entity.solve()
+
+if __name__ == "__main__":
+    main()
